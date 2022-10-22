1746fcbdaa43f71f07700ca8fa75505c710ba3b3
Mohsen <sed.mohsen.moosavi@gmail.com>
2022-10-22 16:07:06 +0330

    add initial top navbar / add logos

---
+++ src/components/app/MainMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="w-100 py-3 bg-slate-50 shadow sticky top-0">
+    <nav class="mx-5 flex items-center">
+      <a href="/">
+        <MainLogo class="h-[32px]" />
+      </a>
+      <div class="grow"></div>
+      <div>
+        <LinkButton type="light mx-2">Sign up</LinkButton>
+        <LinkButton type="primary">log in</LinkButton>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import LinkButton from '@/components/ui/forms/LinkButton.vue'
+import MainLogo from './MainLogo.vue'
+</script>
+
+<style>
+  .cls-1{fill:red;}
+  .cls-2{fill:#fff;}
+  .cls-3{fill:url(#linear-gradient);}
+  .cls-4{fill:url(#linear-gradient-2);}
+</style>
---
+++ src/components/ui/forms/LinkButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :class="classes"><slot /></a>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+const props = defineProps({
+  type: { type: String, default: 'primary'}
+})
+const classes = computed(() => ([
+  `button-${props.type}`
+]))
+</script>
+
+<style lang="postcss" scoped>
+.button {
+  @apply cursor-pointer inline-block px-6 py-2.5  font-medium text-xs leading-tight uppercase rounded shadow hover:shadow-md focus:shadow-md focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out
+}
+.button-primary {
+  @apply button bg-indigo-700 text-white hover:bg-indigo-600
+}
+.button-secondary {
+  @apply button bg-slate-600 text-white hover:bg-slate-500
+}
+.button-light {
+  @apply button bg-slate-100 text-slate-800 hover:bg-slate-50
+}
+</style>
