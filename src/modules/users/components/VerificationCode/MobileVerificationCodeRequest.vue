5bfefdf7802bb92074b4c1ef648a9042fcc450a1
Mohsen <sed.mohsen.moosavi@gmail.com>
2022-10-26 20:37:50 +0330

    add log in link to register page / add terms and policy links to register page

--- src/modules/users/components/VerificationCode/MobileVerificationCodeRequest.vue
+++ src/modules/users/components/VerificationCode/MobileVerificationCodeRequest.vue
@@ -5,9 +5,12 @@
     <router-link to="/" class="block">
       <MainLogo class="h-12" />
     </router-link>
+
+    <h1 class="mt-5 text-lg font-bold">{{ $t('Create your Hapigan Account') }}</h1>
+    <div class="text-sm">{{ $t('By continuing, you are agree to our') }} <a class="link" href="/terms" target="_blank">{{ $t('Terms of Service') }}</a> {{ $t('and')}} <a class="link" href="/privacy" target="_blank">{{ $t('Privacy Policy') }}</a></div>
     <TextInput
       v-model="mobile"
-      class="mt-10"
+      class="mt-5"
       :validator="mobileValidator"
       :label="$t('Phone number')"
       :placeholder="$t('+9891xxxxxxxx')" />
@@ -18,6 +21,8 @@
       :full="true"
       @click="onClick"
       :is-processing="isProcessing">Send verification code</ActionButton>
+    
+    <div class="mt-5">{{ $t('Already have an account?') }} <router-link class="link" to="/login"><strong>{{ $t('Log in')}}</strong></router-link></div>
   </div>
 </template>
 
